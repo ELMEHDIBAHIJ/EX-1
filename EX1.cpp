c32656eac95c170234d862e75ade668619f3c122
ELMEHDIBAHIJ <bahijstudies@gmail.com>
2022-09-07 14:18:29 +0100

    first try

---
+++ EX1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c ;
+	printf("write first numb") ;
+	scanf("%d",&a) ;
+	printf("write second numb");
+	scanf("%d",&b);
+	c = a / b;
+	printf("%d / %d = %d\n", a, b, c);
+	
+	
+	return 0 ;
+}
+
