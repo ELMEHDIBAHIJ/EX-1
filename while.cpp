99d9999fc658d594685842c042bbc425c650e077
ELMEHDIBAHIJ <bahijstudies@gmail.com>
2022-09-08 09:57:32 +0100

    while

---
+++ while.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+	int f;
+	printf("type your age");
+	scanf("%d",&f);
+	while ( f > 18 );
+	  { printf("minors are not aloud here");
+	  }
+	return 0 ;
+}
