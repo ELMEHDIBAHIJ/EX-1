0bd5ac0ce1a10f8e033be1b2259b300796b11829
ELMEHDIBAHIJ <bahijstudies@gmail.com>
2022-09-09 17:20:15 +0100

    first test

---
+++ firsttest.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	
+	
+	int a,b,c;
+	
+	printf("type numb a\n");
+	scanf("%d",&a);
+	printf("type numb b\n");
+	scanf("%d",&b);
+	 c = pow(a,b);
+	printf("a puissance b = %d\n",c);
+	for(a;a>0;a=a-2)
+	{
+	   
+		}
+		if(a==0){
+	   	printf("a est pair\n");
+	   }else
+	   if(a==-1){
+	   	printf("a est impair\n");
+	   }
+	for(b;b>0;b=b-2)	
+     {
+     	
+	 }
+	   		if(b==0){
+	   	printf("b est pair\n");
+	   }else
+	   if(b==-1){
+	   	printf("b est impair\n");
+	   }
+	  
+	  
+	return 0;
+	
+}
